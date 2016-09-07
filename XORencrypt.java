617cb86d58ac9e7d7cd5d90b89c05084d9ddfe41
Vince K <vjkurtz@gmail.com>
2016-09-07 00:36:32 -0400

    fixed whitespace (thanks eclipse)

--- XORencrypt.java
+++ XORencrypt.java
@@ -13,54 +13,54 @@ public class XORencrypt {
     // Set encoding for the message
     static Charset encoding = StandardCharsets.US_ASCII;
 
-	public static void main(String[] args) {
-		try {
-			if ( args[0].toLowerCase().equals("readtest")) {
-				readTest(args[1]);
-			} else if ( args[0].equals("xor")) {
-				xor(args[1], args[2]);
-			} else if (args[0].equals("analyze")) {
-				analyze(args[1], args[2]);
-			} else {
-				help();
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
-			// Fallback to the help function if args aren't right
+    public static void main(String[] args) {
+        try {
+            if ( args[0].toLowerCase().equals("readtest")) {
+                readTest(args[1]);
+            } else if ( args[0].equals("xor")) {
+                xor(args[1], args[2]);
+            } else if (args[0].equals("analyze")) {
+                analyze(args[1], args[2]);
+            } else {
+                help();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // Fallback to the help function if args aren't right
             System.out.println("ERROR: incorrect arguments.\n");
-			help();
+            help();
             e.printStackTrace();
-		}
-	}
+        }
+    }
 
-	public static void help() {
+    public static void help() {
         // Print basic usage information
-		System.out.println("Usage:\n"
-				+ "    help -- get help\n"
-				+ "    readTest <filepath> -- cat a file\n"
-				+ "    xor <filepath> <cipher> -- XOR text in file with cipher\n"
-				+ "    analyze <filepath> <num buckets> -- give character frequencies for text in file for each bucket\n");
-	}
-	
-	public static void readTest(String fpath) {
-		// Print the contents of the specified file to stdout
+        System.out.println("Usage:\n"
+                + "    help -- get help\n"
+                + "    readTest <filepath> -- cat a file\n"
+                + "    xor <filepath> <cipher> -- XOR text in file with cipher\n"
+                + "    analyze <filepath> <num buckets> -- give character frequencies for text in file for each bucket\n");
+    }
+
+    public static void readTest(String fpath) {
+        // Print the contents of the specified file to stdout
         String contents = readFile(fpath);
         System.out.println(contents);
-	}
-	
-	public static void xor(String fpath, String cipher) {
+    }
+
+    public static void xor(String fpath, String cipher) {
         // Encrypt the contents of the given file by XORing each bit with the given cipher.
         // Print the resulting encrypted message to stdout.
 
         // Read the input file
-		String message = readFile(fpath);
+        String message = readFile(fpath);
 
         String encrypted = xorStrings(message, cipher);
 
         System.out.println(encrypted);
-		
-	}
-	
-	public static void analyze(String fpath, String bins) {
+
+    }
+
+    public static void analyze(String fpath, String bins) {
         int N = Integer.parseInt(bins);  
 
         // Load encrypted message from file
@@ -86,34 +86,34 @@ public class XORencrypt {
             common.add( mostCommon(buckets.get(i)) );
         }
         System.out.println(common);
-        
+
         // xor each most common character with " " to get a character in the key
         for (int j=0; j<common.size(); j++) {
             decrypted.add( xorStrings(common.get(j), " ") );
         }
-        
+
         // print those characters which are (probably) in the key
         System.out.println(decrypted);
-	}
-	
+    }
+
     private static String readFile(String fpath) {
-		Scanner data = null;
-		
-		try {
+        Scanner data = null;
+
+        try {
             byte[] encoded = Files.readAllBytes(Paths.get(fpath));
             return new String(encoded, encoding);
-		}
+        }
         catch (Exception e){
-			System.out.println("ERROR: Something went wrong with file reading.\n");
-			e.printStackTrace();
-			System.exit(0);
+            System.out.println("ERROR: Something went wrong with file reading.\n");
+            e.printStackTrace();
+            System.exit(0);
             return "failed";  // java seems to require something to be returned even here. Is there a more elegant way?
-		}
+        }
     }
 
     private static String mostCommon(String s) {
         // Find and return the most common character in the given (ASCII) string.
-        
+
         String most_common;
         int count[] = new int[128];  // there are 128 ASCII characters
         int max_count = 0;
